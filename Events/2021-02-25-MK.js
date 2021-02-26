30215d48d2d99a078a65f38b5f7c64d41ec514fc
Not a Real Name <dummmy@dummy.com>
2021-02-25 19:36:00 -0800

    Prettify

--- Events/2021-02-25-MK.js
+++ Events/2021-02-25-MK.js
@@ -29,22 +29,20 @@ const SELECT_LOCATION = "middle";
 
 function executeTurnFunction(turn) {
     if(turn == 1) {
-        // Vanille break
+        // breaker
         selectAbilities(1, [{x: 6, y: 1}, {x: 10, y: 1}, {x: 11, y: 0}])
 
+        // DPS
         selectAbilities(5, [{x: 0, y: 0}])
         selectAbilities(2, [{x: 5, y: 1}, {x:7, y: 1}, {x: 7, y: 1}, {x: 7, y: 1}])
         selectAbilities(3, [{x: 6, y: 0}])
 
-        activateUnit(1);
-        sleep(1);
-
+        activateUnit(1); sleep(1);
         _.forEach(_.range(2,7), function(i) {activateUnit(i)});
     } else {
         pressReload();
         
-        activateUnit(1);
-        sleep(1);
+        activateUnit(1); sleep(1);
 
         _.forEach(_.range(2,7), function(i) {activateUnit(i)});
     }
